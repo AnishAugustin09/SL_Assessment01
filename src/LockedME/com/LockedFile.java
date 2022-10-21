afb70c80c6d6d07479b58f273c9446f225d472bd
AnishAugustin09 <anishaugustin09@gmail.com>
2022-10-21 19:14:52 +0530

    added add & delete method

--- src/LockedME/com/LockedFile.java
+++ src/LockedME/com/LockedFile.java
@@ -3,6 +3,7 @@ package LockedME.com;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
+import java.io.IOException;
 public class LockedFile {
 	
 	static File fileName;
@@ -40,7 +41,7 @@ public class LockedFile {
 		
 	}
 
-	private static void manageOperation() {
+	private static void manageOperation() throws IOException {
 		// TODO Auto-generated method stub
 		DisplayRecords.displayOperationRecords();
 		Scanner sc=new Scanner(System.in);
@@ -69,13 +70,38 @@ public class LockedFile {
 		
 	}
 
-	private static void deleteFile() {
+	private static void deleteFile() throws IOException {
 		// TODO Auto-generated method stub
+		System.out.println("Enter the File Name to be deleted ");
+		Scanner sc= new Scanner(System.in);
+		String delFile=sc.next();
+		String[] list= fileName.list();
+		for(String file:list) {
+			if(delFile.equals(file)) {
+				fileName.delete();
+				System.out.println("File "+delFile+" has Successfully deleted");
+			}else {
+				System.out.println(" File Not Found, Please enter the right spelling");
+				manageOperation();
+			}
+				
+		}
 		
 	}
 
-	private static void addFile() {
+	private static void addFile() throws IOException {
 		// TODO Auto-generated method stub
+		System.out.println("Enter the file name");
+		Scanner sc = new Scanner(System.in);
+		String fName= sc.next();
+		File file= new File(fileName+"/"+ fName);
+		if(!file.exists()) {
+			file.createNewFile();
+			System.out.println("File Creted");
+		}else {
+			System.out.println("NOTE - !File Name "+ fName +"already exist, Enter a new name");
+			addFile();
+		}
 		
 	}
 
