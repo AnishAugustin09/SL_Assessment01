618860e6425205c60f024c60116980310b401375
AnishAugustin09 <anishaugustin09@gmail.com>
2022-10-21 11:06:19 +0530

    added new methods

--- src/LockedME/com/LockedFile.java
+++ src/LockedME/com/LockedFile.java
@@ -1,5 +1,50 @@
 package LockedME.com;
 
+import java.util.Scanner;
+import java.io.File;
 public class LockedFile {
+	
+	static File fileName;
 
+	public static void fileCreation() {
+		String Path = System.getProperty("user.dir");
+		fileName = new File(Path+ "/ Master");
+		if(!fileName.exists()) {
+			fileName.mkdirs();
+			
+		}
+	}
+	
+	public static void beginingOfProgram() {
+		
+		fileCreation();
+		
+		DisplayRecords.initialOperationRecords();
+		Scanner sc=new Scanner (System.in);
+		int userOpt=sc.nextInt();
+		switch(userOpt) {
+		case 1: filesinFolder();
+				break;
+		case 2: manageOperation();
+				break;
+		case 3: programEnds();
+				break;
+		}
+		
+	}
+
+	private static void programEnds() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void manageOperation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void filesinFolder() {
+		// TODO Auto-generated method stub
+		
+	}
 }
