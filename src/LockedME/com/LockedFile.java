edcbb13cbb70fc8bba15f0fa03f285f39d0b6dad
AnishAugustin09 <anishaugustin09@gmail.com>
2022-10-22 15:28:30 +0530

    Program Test 1

--- src/LockedME/com/LockedFile.java
+++ src/LockedME/com/LockedFile.java
@@ -6,24 +6,25 @@ import java.io.File;
 import java.io.IOException;
 public class LockedFile {
 	
-	static File fileName;
+	static File fileName;;
+	static String Path;
 
 	public static void fileCreation() {
-		String Path = System.getProperty("user.dir");
+		Path = System.getProperty("user.dir");
 		fileName = new File(Path+ "/ Master");
 		if(!fileName.exists()) {
-			fileName.mkdirs();
-			
+			fileName.mkdirs();	
 		}
 	}
 	
-	public static void beginingOfProgram() {
+	public static void beginingOfProgram() throws IOException {
 		
 		fileCreation();
 		
 		DisplayRecords.initialOperationRecords();
 		Scanner sc=new Scanner (System.in);
 		int userOpt=sc.nextInt();
+		
 		switch(userOpt) {
 		case 1: filesinFolder();
 				break;
@@ -31,18 +32,22 @@ public class LockedFile {
 				break;
 		case 3: programEnds();
 				break;
+		default: System.out.println("Invalid option, Please enter the right option");
 		}
 		
 	}
 
 	private static void programEnds() {
 		// TODO Auto-generated method stub
+		System.out.println("Thank You");
+		System.exit(0);
 		
 		
 	}
 
 	private static void manageOperation() throws IOException {
 		// TODO Auto-generated method stub
+		fileCreation();
 		DisplayRecords.displayOperationRecords();
 		Scanner sc=new Scanner(System.in);
 		int userOpt= sc.nextInt();
@@ -53,20 +58,30 @@ public class LockedFile {
 				break;
 		case 3: searchFile();
 				break;
-		case 4: goBack();
+		case 4: beginingOfProgram();
 				break;
 		default:System.out.println("Invalid Option, Please enter the right one");
 		}
 		
 	}
 
-	private static void goBack() {
+	private static void searchFile() throws IOException {
 		// TODO Auto-generated method stub
-		
-	}
-
-	private static void searchFile() {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the File Name");
+		String fileSearch = sc.nextLine();
+		String[] list=fileName.list();
+		for(String file:list) {
+			if(fileSearch.equals(file)) {
+				System.out.println(fileSearch);
+				System.out.println("File Found");
+				System.out.println();
+				manageOperation();
+			}else {
+				System.out.println("No Such file in the directory, Please check the spelling before you enter");
+				manageOperation();
+			}
+		}
 		
 	}
 
@@ -74,18 +89,23 @@ public class LockedFile {
 		// TODO Auto-generated method stub
 		System.out.println("Enter the File Name to be deleted ");
 		Scanner sc= new Scanner(System.in);
-		String delFile=sc.next();
+		String delFile=sc.nextLine();
+		File file=new File(fileName+"/"+delFile);
 		String[] list= fileName.list();
-		for(String file:list) {
-			if(delFile.equals(file)) {
-				fileName.delete();
-				System.out.println("File "+delFile+" has Successfully deleted");
-			}else {
-				System.out.println(" File Not Found, Please enter the right spelling");
-				manageOperation();
-			}
+		for(String files: list) {
+			if(delFile.equals(files)&&file.delete()){
 				
+				System.out.println("File "+delFile+" has Successfully deleted");
+				System.out.println();
+				manageOperation();
+			}else {
+				System.out.println("File Not Found, Please enter the right spelling");
+				manageOperation();
+			}	
 		}
+		
+				
+		
 		
 	}
 
@@ -93,11 +113,12 @@ public class LockedFile {
 		// TODO Auto-generated method stub
 		System.out.println("Enter the file name");
 		Scanner sc = new Scanner(System.in);
-		String fName= sc.next();
+		String fName= sc.nextLine();
 		File file= new File(fileName+"/"+ fName);
 		if(!file.exists()) {
 			file.createNewFile();
-			System.out.println("File Creted");
+			System.out.println("File Created");
+			manageOperation();
 		}else {
 			System.out.println("NOTE - !File Name "+ fName +"already exist, Enter a new name");
 			addFile();
@@ -105,13 +126,22 @@ public class LockedFile {
 		
 	}
 
-	private static void filesinFolder() {
+	private static void filesinFolder() throws IOException {
 		// TODO Auto-generated method stub
-		String[] list= fileName.list();
-		for(String files : list) {
+		if (fileName.list().length > 0) {
+			String[] list= fileName.list();
 			Arrays.sort(list);
-			System.out.println(files);
+			for(String files : list) {
+				System.out.println(files);
+				}
+			System.out.println("Files are displayed");
+			beginingOfProgram();
+		}else {
+			System.out.println("Directory is empty");
+			beginingOfProgram();
+			
 		}
+		
 		
 	}
 }
--- src/LockedME/com/LockedMain.java
+++ src/LockedME/com/LockedMain.java
@@ -1,12 +1,16 @@
 package LockedME.com;
 
+import java.io.IOException;
+
 public class LockedMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		// Calling a method to display the application and the developer details
 		DisplayRecords.entryRecords("Anish Augustin");
 		
+		
+		LockedFile.beginingOfProgram();
 		
 	}
 
